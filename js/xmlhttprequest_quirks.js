753e085784787c88511a077c743cde6d75fa8c3f
Athanasios Oikonomou <athoik@gmail.com>
2022-02-13 21:02:30 +0200

    quirks: check if globals exist and extend hack to left/right

--- js/xmlhttprequest_quirks.js
+++ js/xmlhttprequest_quirks.js
@@ -26,10 +26,11 @@ window.addEventListener('load', function() {
   //check if we are on ert.gr/skai/mega and try to fix menu up/down
   let href = window.location.href;
   if (href.indexOf(".ert.gr") > -1 || href.indexOf("skai.smart-tv-data.com") > -1 || href.indexOf("mega.smart-tv-data.com") > -1) {
+    if(typeof GLOBALS == 'undefined') return;
     console.info('[MENUFIX] Trying to fix double key up/down');
     document.removeEventListener(GLOBALS.keyevent, GLOBALS.keyeventlistener, false);
     GLOBALS.keyeventlistener = function (e) {
-      if(!e.isTrusted && (e.keyCode == VK_DOWN||e.keyCode == VK_UP)) return;
+      if(!e.isTrusted && (e.keyCode == VK_DOWN||e.keyCode == VK_UP||e.keyCode == VK_LEFT||e.keyCode == VK_RIGHT)) return;
       if (GLOBALS.demomode && (e.ctrlKey || e.altKey || e.metaKey)) return;
       var kc = e.keyCode, handled;
       if (!kc) kc = e.charCode;
